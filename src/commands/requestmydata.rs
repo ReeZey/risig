4aca26b4d742fbce3be3bc10885e9ac91dcc08f9
ReeZey <johannes@seved.net>
2023-07-03 15:09:45 +0200

    requestmydata bug

--- src/commands/requestmydata.rs
+++ src/commands/requestmydata.rs
@@ -10,9 +10,12 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
 pub(crate) async fn run(command: &mut ApplicationCommandInteraction, ctx: &Context, user: User, user_data: Document) {
     let bson_data: Bson = user_data.into();
     let json_data = bson_data.into_relaxed_extjson();
-    let pretty_json_data: String = serde_json::to_string_pretty(&json_data).unwrap();
+    let mut pretty_json_data: String = serde_json::to_string_pretty(&json_data).unwrap();
+    if pretty_json_data.len() > 2000 {
+        pretty_json_data = format!("{}{}", &pretty_json_data[..1997], "...");
+    }
     
     //send_command_response(command, &ctx, &format!("```json\n{}```", pretty_json_data), MessageFlags::EPHEMERAL).await;
-    send_file_command_response(command, &ctx, &pretty_json_data[..2000], (pretty_json_data.as_bytes().to_vec(), &format!("{}.json", user.name)), MessageFlags::EPHEMERAL).await;
+    send_file_command_response(command, &ctx, &pretty_json_data, (pretty_json_data.as_bytes().to_vec(), &format!("{}.json", user.name)), MessageFlags::EPHEMERAL).await;
     //send_file(&ctx, &msg, Some(), pretty_json_data.as_bytes().to_vec(), &format!("{}.json", msg.author.id)).await;
 }
